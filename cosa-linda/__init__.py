df3f32397d6a3c3909349bcf24769d598b327002
Patrick José Pereira <patrickelectric@gmail.com>
2019-10-09 17:51:58 -0300

    Add initial translation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- cosa-linda/__init__.py
+++ cosa-linda/__init__.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 
 print("""O Zen do Python, por Tim Peters e xuxcado pelos Manezinhos da Ilha.
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
+Uma gracinha é melhor que um exconmugado.
+Tú tem que amostrar ô.
 Simple is better than complex.
 Complex is better than complicated.
 Flat is better than nested.
@@ -14,13 +14,13 @@ Sparse is better than dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
+Essa lambança ai ô, da não.
+Ninguém vai ve, ta safo.
 In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
+Tu câmba ali, adepösh daí é só reto toda vida.
 Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
+Melhor dijaöje dû qui mofar com a pomba na balaia.
 Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!""")
+Óh a maçaroca, ta matado.
+Ta uma caupa quirídu, és um mônshtru.
+Hohohoho chamadô espaçado -- um baita feio!""")
